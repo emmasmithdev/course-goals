46bcd0161d08af2eafcdb8d29f56867b3e5b9a73
Emma <emmagray183@gmail.com>
2020-03-10 14:44:48 +0000

    GoalInput and GoalItem components

--- App.js
+++ App.js
@@ -1,33 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, 
+        Text, 
+        View, 
+        TextInput, 
+        Button, 
+        ScrollView, 
+        FlatList } 
+        from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: goalTitle }]);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" 
-        style={styles.input} 
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
-      <ScrollView >
-        {courseGoals.map((goal) => <View key={goal} style={styles.listItem}>
-          <Text>{goal}</Text>
-          </View>)}
-      </ScrollView>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+    />    
     </View>
   );
 }
@@ -35,24 +33,6 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 40
-  },
-  inputContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: "center"
-  },
-  input: {
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: "#ccc",
-    borderColor: 'black',
-    borderWidth: 1
   }
 });
 
---
+++ components/GoalItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet, Touchable } from 'react-native';
+
+const GoalItem = props => {
+    return (
+    <Touchable>
+        <View style={styles.listItem}>
+            <Text>{props.title}</Text>
+        </View>
+    </Touchable>
+    );
+}
+
+const styles = StyleSheet.create({
+    listItem: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: "#ccc",
+        borderColor: 'black',
+        borderWidth: 1
+      }
+});
+
+export default GoalItem;
